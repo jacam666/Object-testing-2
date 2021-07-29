23e63b71c3c97b8562d487b62eff40fa383accec
jacam666 <ja6cam@gmail.com>
2021-07-29 22:01:07 +0200

    Add project files.

---
+++ Object testing 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Object_testing_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name.");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine($"Hi " + name + " and welcome to your personalised workout plan");
+            
+            ChestWorkout workOutPlan = new();
+            //Exercises workOutExercises = new();
+            workOutPlan.NumberOfSets = 20;
+            workOutPlan.NumberOfRepsPerSets = 10;
+            workOutPlan.RestBetweenSets = 60;
+            //workOutExercises.FlatBench = firstExercise;
+
+            Console.WriteLine("The number of sets that are required are " + workOutPlan.NumberOfSets + " sets per workout");
+            Console.WriteLine("The amount of Reps per set are " + workOutPlan.NumberOfRepsPerSets);
+            Console.WriteLine("You are allowed " + workOutPlan.RestBetweenSets + " seconds rest between sets.");
+        }
+    }
+    public class ChestWorkout
+    {
+        public int NumberOfSets;
+        public int NumberOfRepsPerSets;
+        public int RestBetweenSets;
+        
+
+    }
+    public class Exercises
+    {
+ 
+        public string FlatBench;
+        public string InclineBench;
+        public string CableFlyes;
+        public string InclineDumbbellPress;
+        public string PecDec;
+    }
+}
