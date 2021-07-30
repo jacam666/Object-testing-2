176fc3585f23c97ee2c2131fd16cde2a6d2a6f4e
jacam666 <ja6cam@gmail.com>
2021-07-30 14:46:39 +0200

    trying to add exercises to the code but getting stuck

--- Object testing 2/Program.cs
+++ Object testing 2/Program.cs
@@ -4,12 +4,27 @@ namespace Object_testing_2
 {
     class Program
     {
+        //public static string InclineBench { get; set; }
+        //public static string FlatBench { get; set; }
+        //public static string CableFlyes { get; set; }
+        //public static string InclineDumbbellFlyes { get; set; }
+        //public static string PecDec { get; set; }
+
         static void Main(string[] args)
         {
             UI.DisplayWelcomeScreen();
                        
             UI.RequiredNumbersAndTimings();
-                     
+
+            Exercises WorkoutExercises = new();
+            
+            WorkoutExercises.FirstExercise = 
+            WorkoutExercises.SecondExercise = 
+            WorkoutExercises.ThirdExercise = 
+            WorkoutExercises.FourthExercise = 
+            WorkoutExercises.FifthExercise = 
+
+            //Console.WriteLine("Your first exercise is : " + WorkoutExercises.SecondExercise);
         }
     }
     public class ChestWorkout
@@ -22,11 +37,10 @@ namespace Object_testing_2
     }
     public class Exercises
     {
- 
-        public string FlatBench;
-        public string InclineBench;
-        public string CableFlyes;
-        public string InclineDumbbellPress;
-        public string PecDec;
+        public string FirstExercise;
+        public string SecondExercise;
+        public string ThirdExercise;
+        public string FourthExercise;
+        public string FifthExercise;
     }
 }
