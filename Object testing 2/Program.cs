0911cf3bb1bf5fe9d4986f5f2496692d06a886b2
jacam666 <ja6cam@gmail.com>
2021-07-30 18:37:12 +0200

    : added more instructions to the console writeline regarding the  exercises

--- Object testing 2/Program.cs
+++ Object testing 2/Program.cs
@@ -11,18 +11,20 @@ namespace Object_testing_2
                        
             UI.RequiredNumbersAndTimings();
             ChestWorkout workOutPlan = new();
-            
+
+            workOutPlan.NumberOfSets = 20;
+            workOutPlan.NumberOfRepsPerSets = 10;
             workOutPlan.FirstExercise = "Bench Press";
             workOutPlan.SecondExercise = "Incline Bench";
             workOutPlan.ThirdExercise = " Cable Flyes";
             workOutPlan.FourthExercise = " Incline Dumbbell Flyes";
             workOutPlan.FifthExercise = " Pec Dec";
 
-            Console.WriteLine("First exercise to complete : " + workOutPlan.FirstExercise);
-            Console.WriteLine("Second exercise to complete : " + workOutPlan.SecondExercise);
-            Console.WriteLine("Third exercise to complete : " + workOutPlan.ThirdExercise);
-            Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise);
-            Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise);
+            Console.WriteLine("First exercise to complete : " + workOutPlan.FirstExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Second exercise to complete : " + workOutPlan.SecondExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Third exercise to complete : " + workOutPlan.ThirdExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
 
             
             //Console.WriteLine("Your first exercise is : " + WorkoutExercises.SecondExercise);
