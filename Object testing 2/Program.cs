12260bc0e284c488a75f9947c6b214e24dbd6a19
jacam666 <ja6cam@gmail.com>
2021-07-30 18:43:04 +0200

    : created an exercisesToComplete ui method

--- Object testing 2/Program.cs
+++ Object testing 2/Program.cs
@@ -7,27 +7,9 @@ namespace Object_testing_2
 
         static void Main(string[] args)
         {
-            UI.DisplayWelcomeScreen();
-                       
+            UI.DisplayWelcomeScreen();                     
             UI.RequiredNumbersAndTimings();
-            ChestWorkout workOutPlan = new();
-
-            workOutPlan.NumberOfSets = 20;
-            workOutPlan.NumberOfRepsPerSets = 10;
-            workOutPlan.FirstExercise = "Bench Press";
-            workOutPlan.SecondExercise = "Incline Bench";
-            workOutPlan.ThirdExercise = " Cable Flyes";
-            workOutPlan.FourthExercise = " Incline Dumbbell Flyes";
-            workOutPlan.FifthExercise = " Pec Dec";
-
-            Console.WriteLine("First exercise to complete : " + workOutPlan.FirstExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
-            Console.WriteLine("Second exercise to complete : " + workOutPlan.SecondExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
-            Console.WriteLine("Third exercise to complete : " + workOutPlan.ThirdExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
-            Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
-            Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
-
-            
-            //Console.WriteLine("Your first exercise is : " + WorkoutExercises.SecondExercise);
+            UI.ExercicesToComplete();           
         }
     }
     public class ChestWorkout
--- Object testing 2/UI.cs
+++ Object testing 2/UI.cs
@@ -34,6 +34,24 @@ namespace Object_testing_2
             Console.WriteLine("You are allowed " + workOutPlan.RestBetweenSets + " seconds rest between sets.");
         }
 
+        public static void ExercicesToComplete()
+        {
+            ChestWorkout workOutPlan = new();
+
+            workOutPlan.NumberOfSets = 20;
+            workOutPlan.NumberOfRepsPerSets = 10;
+            workOutPlan.FirstExercise = "Bench Press";
+            workOutPlan.SecondExercise = "Incline Bench";
+            workOutPlan.ThirdExercise = " Cable Flyes";
+            workOutPlan.FourthExercise = " Incline Dumbbell Flyes";
+            workOutPlan.FifthExercise = " Pec Dec";
+
+            Console.WriteLine("First exercise to complete : " + workOutPlan.FirstExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Second exercise to complete : " + workOutPlan.SecondExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Third exercise to complete : " + workOutPlan.ThirdExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+        }
 
     }
 
