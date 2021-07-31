3877f2063c85f30d6411051fbc52899e79228f89
jacam666 <ja6cam@gmail.com>
2021-07-31 20:59:00 +0200

    : added ui method for back workout

--- Object testing 2/UI.cs
+++ Object testing 2/UI.cs
@@ -95,6 +95,23 @@ namespace Object_testing_2
             Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
             Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
         }
+        public static void BackExercisesToComplete()
+        {
+            BackWorkout workOutPlan = new();
+            workOutPlan.NumberOfSets = 20;
+            workOutPlan.NumberOfRepsPerSets = 10;
+            workOutPlan.FirstExercise = " Barbell BentOver Rows";
+            workOutPlan.SecondExercise = " Cable Lat Pulldowns";
+            workOutPlan.ThirdExercise = " Cable Close Grip Pulldowns";
+            workOutPlan.FourthExercise = " Seated Cable Rows";
+            workOutPlan.FifthExercise = " BodyWeight Pull Ups";
+
+            Console.WriteLine("First exercise to complete : " + workOutPlan.FirstExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Second exercise to complete : " + workOutPlan.SecondExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Third exercise to complete : " + workOutPlan.ThirdExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+        }
             
 
         
