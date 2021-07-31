c4f51d3564be550a8a50bac4154cfd970f963169
jacam666 <ja6cam@gmail.com>
2021-07-31 20:44:04 +0200

    : added back workout class

--- Object testing 2/Program.cs
+++ Object testing 2/Program.cs
@@ -7,7 +7,7 @@ namespace Object_testing_2
 
         static void Main(string[] args)
         {
-            UI.DisplayWelcomeScreen();                     
+            UI.DisplayWelcomeScreen();
             UI.RequiredNumbersAndTimings();
             Console.WriteLine("CHEST WORKOUT :");
             UI.ChestExercisesToComplete();
@@ -15,9 +15,25 @@ namespace Object_testing_2
             UI.LegExercisesToComplete();
             Console.WriteLine("Shoulder Workout");
             UI.ShoulderExercisesToComplete();
+
+            BackWorkout workOutPlan = new();
+            workOutPlan.NumberOfSets = 20;
+            workOutPlan.NumberOfRepsPerSets = 10;
+            workOutPlan.FirstExercise = " Barbell BentOver Rows";
+            workOutPlan.SecondExercise = " Cable Lat Pulldowns";
+            workOutPlan.ThirdExercise = " Cable Close Grip Pulldowns";
+            workOutPlan.FourthExercise = " Seated Cable Rows";
+            workOutPlan.FifthExercise = " BodyWeight Pull Ups";
+
+            Console.WriteLine("First exercise to complete : " + workOutPlan.FirstExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Second exercise to complete : " + workOutPlan.SecondExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Third exercise to complete : " + workOutPlan.ThirdExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fourth exercise to complete : " + workOutPlan.FourthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+            Console.WriteLine("Fifth exercise to complete : " + workOutPlan.FifthExercise + " : " + workOutPlan.NumberOfSets + " sets of " + workOutPlan.NumberOfRepsPerSets + " reps per set.");
+
         }
     }
-    
+
     public class ChestWorkout
     {
         public int NumberOfSets;
@@ -29,7 +45,7 @@ namespace Object_testing_2
         public string FourthExercise;
         public string FifthExercise;
     }
-   
+
     public class LegWorkout
     {
         public int NumberOfSets;
@@ -42,6 +58,17 @@ namespace Object_testing_2
         public string FifthExercise;
     }
     public class ShoulderWorkout
+    {
+        public int NumberOfSets;
+        public int NumberOfRepsPerSets;
+        public int RestBetweenSets;
+        public string FirstExercise;
+        public string SecondExercise;
+        public string ThirdExercise;
+        public string FourthExercise;
+        public string FifthExercise;
+    }
+    public class BackWorkout
     {
         public int NumberOfSets;
         public int NumberOfRepsPerSets;
